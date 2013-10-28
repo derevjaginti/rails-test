a8153f2e506f461c16e3582291e9339f83cd8e7a
derevjaginti <derevjaginti@gmail.com>
2013-10-28 21:21:54 +0300

    Created posts with scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131027181336) do
+ActiveRecord::Schema.define(:version => 20131028121354) do
+
+  create_table "posts", :force => true do |t|
+    t.string   "user"
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
