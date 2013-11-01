c4caed12fe50adc7ca93dd09dc608a61e14a24ba
derevjaginti <derevjaginti@gmail.com>
2013-11-01 21:56:17 +0300

    tagged_url removed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,4 @@
 class Post < ActiveRecord::Base
   attr_accessible :id, :user, :body, :title, :created_at, :tag_list
-  unless (ARGV & ['assets:precompile', 'assets:clean']).any?
-    acts_as_taggable_on :tags
-  end
+  acts_as_taggable_on :tags
 end
