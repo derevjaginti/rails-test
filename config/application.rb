7c2a738bdd97b2b97927a8682e1511cf0a0e297b
derevjaginti <derevjaginti@gmail.com>
2013-11-05 21:00:37 +0300

    Added simple rspec tests to Post model,controller

--- config/application.rb
+++ config/application.rb
@@ -13,16 +13,6 @@ end
 module RailsTest
 	class Application < Rails::Application
 
-		config.generators do |g|
-			g.test_framework :rspec, fixture: true
-			g.fixture_replacement :factory_girl, dir: 'spec/factories'
-			g.view_specs false
-			g.helper_specs false
-			g.stylesheets = false
-			g.javascripts = false
-			g.helper = false
-		end
-
 		config.to_prepare do
 			Devise::SessionsController.layout "guest_layout"
 			Devise::RegistrationsController.layout proc{ |controller| user_signed_in? ? "user_layout" : "guest_layout" }
