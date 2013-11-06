6faa1904e05ec94a97b26febabfab68977c8629d
Artjom Juferov <derevjaginti@gmail.com>
2013-11-06 21:14:57 +0200

    Update 20131031063159_create_comments.rb

--- db/migrate/20131031063159_create_comments.rb
+++ db/migrate/20131031063159_create_comments.rb
@@ -1,9 +1,9 @@
 class CreateComments < ActiveRecord::Migration
   def up
     create_table :comments do |t|
-    	t.string :user, :null => false 
-    	t.string :post, :null => false
-    	t.text :body, :null => false 
+      t.string :user, :null => false 
+      t.string :post, :null => false
+      t.text :body, :null => false 
       t.timestamps
     end
   end
