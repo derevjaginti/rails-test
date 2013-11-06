60b37501704ab42c2634526d562deebca6fc0506
Artjom Juferov <derevjaginti@gmail.com>
2013-11-06 21:11:01 +0200

    Update comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ActiveRecord::Base
   attr_accessible :user, :post, :body
 
   def self.who_is(user)
-  	user ? user.username : "Anonymous"
+    user ? user.username : "Anonymous"
   end
 end
